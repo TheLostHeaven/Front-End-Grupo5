ea6e575bd2418a7766bd00ae96c59682b8b92b6a
Daniel <Dannyking07@outlook.es>
2024-10-29 05:18:15 -0500

    feat (src/app/layout -> layout.component): add layout.component.ts

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.sass',
+})
+export class layoutComponent {
+  title = 'Front-AC';
+
+}
