7c54f073af449682c99042669230f368f0573b3c
Daniel <Dannyking07@outlook.es>
2024-11-16 23:34:17 -0500

    update (layout-routing.module.ts)

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from '@shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('../features/Reporting-System/presentation/reporting-system.module').then(m => m.ReportingSystemModule) },
-  {path: '', loadChildren: () => import ('../features/Auth/presentation/auth.module').then(m => m.AuthModule)},
+  {path: '', loadChildren: () => import('../features/reporting-system/presentation/reporting-system.module').then(m => m.ReportingSystemModule) },
+  {path: '', loadChildren: () => import ('../features/auth/presentation/auth.module').then(m => m.AuthModule)},
   {path: '**', component: PageNotFoundComponent}
 ];
 
