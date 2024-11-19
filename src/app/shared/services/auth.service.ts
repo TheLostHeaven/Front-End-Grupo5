ca90d35017a5aad0a4b108c7501d0ce1e57cb610
Daniel <Dannyking07@outlook.es>
2024-11-19 16:46:59 -0500

    feat (auth.service)

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
+import { LoginInformationEntity } from '@auth/core/entities/login-information.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  }
+
