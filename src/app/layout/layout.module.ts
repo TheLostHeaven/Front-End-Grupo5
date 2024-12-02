581def4497a9a0a9bd29945609dfab76dfad230a
Daniel <Dannyking07@outlook.es>
2024-12-02 15:14:39 -0500

    update (layout) update imports to nav-bar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from '@shared/components/header/header.component';
 import { PageNotFoundComponent } from '@shared/components/page-not-found/page-not-found.component';
 //Import modules
 import { LayoutRoutingModule } from './layout-routing.module';
-import { ReportingSystemModule } from '@reporting-system/presentation/reporting-system.module';
+import { HomeModule } from '@home/presentation/home.module';
 import { AuthModule } from '@auth/presentation/auth.module';
 import { ServiceProviderModule } from '../core/service-providers/service-provider.module';
 import { ApiService } from '@shared/services/api/api.service';
@@ -15,6 +15,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LoadingService } from '@shared/lib/logic';
+import { ImageModule } from 'primeng/image';
 
 @NgModule({
   declarations: [
@@ -28,9 +29,10 @@ import { LoadingService } from '@shared/lib/logic';
     BrowserAnimationsModule,
     NoopAnimationsModule,
     LayoutRoutingModule,
-    ReportingSystemModule,
+    HomeModule,
     AuthModule,
     ServiceProviderModule,
+    ImageModule
   ],
   providers: [
     provideClientHydration(),
