63ef4b2e94095b88a39cb5e405d8e525c0a7ff50
Paola27 <yineth2paola2@gmail.com>
2024-12-20 11:58:55 -0500

    feat (menu component) template for menu

--- src/app/features/forms/presentation/pages/menu/menu.page.ts
+++ src/app/features/forms/presentation/pages/menu/menu.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { MenuConfig } from './menu.config';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,4 +10,43 @@ import { Component } from '@angular/core';
 })
 export class MenuPage {
 
+
+  public config = MenuConfig;
+
+  public items = [
+    {
+      title: 'Postes caidos',
+      route: 'form/fallenpoles',
+      icon: 'assets/poste.svg'
+    },
+    {
+      title: 'Cortes de energía',
+      route: 'form/powerUp',
+      icon: 'assets/corteenergia.svg'
+    },
+    {
+      title: 'Falla de transformador',
+      route: 'form/tranformer',
+      icon: 'assets/fallatranformador.svg'
+    },
+    {
+      title: 'Cables dañados o caidos',
+      route: '/form/damage',
+      icon: 'assets/cablesdañados.svg'
+    },
+  ];
+
+
+
+  constructor(private location: Location,private router: Router) { }
+
+  public selectedComponent: string | null = null;
+
+  public showComponent(component: string): void {
+    this.selectedComponent = component;
+  }
+
+  public setActiveComponent(route: string): void {
+    this.router.navigate([route]);
+  }
 }
